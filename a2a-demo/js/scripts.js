8d0d8b906f3e4227af5009c8dbc5f452fda66d9a
Nanda Harpreet <nanda.harpreet@canada.ca>
2019-12-20 18:12:17 -0500

    reverting back

--- a2a-demo/js/scripts.js
+++ a2a-demo/js/scripts.js
@@ -1,4 +1,4 @@
-/*var keycloak = Keycloak({
+var keycloak = Keycloak({
   url: "https://sso-dev.ised-isde.canada.ca/auth",
   realm: "vc-authn",
   clientId: "idm-vc-demo"
@@ -20,32 +20,7 @@ keycloak
 function logout() {
   keycloak.logout();
 }
-*/
+
 
 // just going to orig code
-
-var keycloak = Keycloak({
-    url: "https://sso-dev.pathfinder.gov.bc.ca/auth",
-    realm: "vlv1c2en",
-    clientId: "law-society-demo"
-  });
-  keycloak
-    .init({ onLoad: "login-required" })
-    .success(function(authenticated) {
-      // alert(authenticated ? "authenticated" : "not authenticated");
-      keycloak.loadUserInfo().success(function(userInfo) {
-        console.log("UserInfo: ", userInfo)
-        document.getElementById("user-info").innerHTML = userInfo.email;
-        document.body.style.display = "";
-      });
-    })
-    .error(function() {
-      alert("failed to initialize");
-    });
-
-  function logout() {
-    keycloak.logout();
-  }
-// test...1111212112
-
 
